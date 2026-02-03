e9622137a0292f1105831b9aa8b904d7516f3052
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2026-02-03 11:02:20 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
 
     // OWNERSHIP
-    let str1 = String::from("Hello");
-    let str2 = str1;
-    println!("String 2: {}", str2);
+   let x:String = String::from("Hello");
+   procss_string(x);
+//    println!("The value of x in main() is: {}", x);
+}
+
+fn procss_string(item:String) {
+    println!("The value of x in process function is: {}", item);
 }
 
 
