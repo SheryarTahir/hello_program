fc7be45478f749b3345c45a3830019d4eeb48989
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2026-02-03 12:40:57 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+/*  
+// Borrowing reference rules with read and write operation
 fn main() {
+    
+    let mut s1:String = String::from("Hello");
 
-    // OWNERSHIP
-   let x:String = String::from("Hello");
-   procss_string(x);
-//    println!("The value of x in main() is: {}", x);
+    let r1 = &s1;
+    println!("r1 = {}", r1);
+
+    let w1 = &mut s1;
+    w1.push_str(" World");
+    println!("w1 = {}", w1);
 }
-
-fn procss_string(item:String) {
-    println!("The value of x in process function is: {}", item);
-}
-
-
-
+*/
