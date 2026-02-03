3aa307070497e2e3c33b9c6dfc7d365d88c6818b
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2026-02-03 10:49:44 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
-const GLOBAL_VARIABLE: u8 = 100;
 fn main() {
 
     // OWNERSHIP
-    let outside_variable: u8 = 5;
+    let str1 = String::from("Hello");
+    let str2 = str1;
+    println!("String 2: {}", str2);
+}
 
-    {
-        let inside_variable: u8 = 15;
-        println!("The inside variable: {}", inside_variable);
-        println!("Outside variable: {}", outside_variable);
-    }
-    println!("The outside variable: {}", outside_variable);
-    print_value();
-}
-fn print_value() {
-    println!("Global valriable: {}", GLOBAL_VARIABLE);
-}
 
 
